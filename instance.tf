3e73a1337a1691d6306b328b416a5553376ac01a
nkonus <nazmiyekonus@gmail.com>
2020-07-26 13:06:54 -0700

    ec2 security added

--- instance.tf
+++ instance.tf
@@ -6,7 +6,10 @@ provider "aws" {
 resource "aws_instance" "myEc2" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
-  count         = "3"
+  key_name      = "ec2KeyPair"
+  subnet_id     = aws_subnet.public_subnet.id
+   associate_public_ip_address = true
+  count         = "1"
   tags = {
     Name = "web_server" 
   }
--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = "vpc-0558eb978b89ca197"
+  vpc_id     = aws_vpc.main.id//"vpc-0558eb978b89ca197"
   cidr_block = "10.0.1.0/24"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet"
   }
